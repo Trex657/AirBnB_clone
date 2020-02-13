23ee6bbf69071ac6a3465077e06c177272773808
Doouh <ake.giraldo@hotmail.com>
2020-02-13 15:27:19 -0500

    Update __init__ method with args and kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -10,10 +10,17 @@ import uuid
 
 class BaseModel():
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if (len(kwargs) > 0):
+            self.id = kwargs['id']
+            self.name = kwargs['name']
+            self.my_number = kwargs['my_number']
+            self.created_at = datetime.strptime(kwargs['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
+            self.updated_at = datetime.strptime(kwargs['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         ans = "["
